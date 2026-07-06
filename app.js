f187f86b4faeeb0d7c8441e3aff2bfdf31f9dddf
disoliveiradso <disoliveira.dso@gmail.com>
2026-07-06 20:21:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1709,7 +1709,7 @@ class LySincApp {
             if (event && event.currentTarget) {
                 event.currentTarget.blur();
             }
-            if (!(false && 'documentPictureInPicture' in window)) {
+            if (!('documentPictureInPicture' in window)) {
                 if (document.pictureInPictureElement) {
                     document.exitPictureInPicture();
                 } else {
